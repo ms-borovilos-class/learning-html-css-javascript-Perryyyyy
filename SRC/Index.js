53ef7b46a01deb5ce34fad84e0b32b18a9560ec8
Perryyyyy <119430557+Perryyyyy@users.noreply.github.com>
2022-11-30 14:33:12 +0000

    My among us

---
+++ SRC/Index.js
@@ -0,0 +1,29 @@
+function  button Alert () {
+     alert(this is an alert)
+}function addition (a, b) {
+   alert)a + b)
+
+ Function subtraction (a, b)) {
+    alert (a - b)
+
+    function multiplacation)a, b) {
+        alert(a * b)
+    
+    Function division(a, b) {
+        alert(a / b)
+    }}
+    Function caculator() {
+       var operation = prompt("enter an operation")
+       var a = promt("enter your number")
+       var b = promt("enter your second number")
+
+       If (operartion ==="addition")
+       alert (aadditon(a,b) = "subtraction"
+       else if (operation === "multiplacation"(a,b)
+       else if (operation == "division")) )
+    
+       alert ("invalid operation")
+       
+
+    }
+ }
